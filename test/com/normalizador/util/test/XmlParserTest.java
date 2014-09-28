2b50ce60cf68e42e61119ca2fd063944fb3fdc77
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2014-09-28 11:41:41 -0500

    Se agrega JUnit y clases de prueba

--- test/com/normalizador/util/test/XmlParserTest.java
+++ test/com/normalizador/util/test/XmlParserTest.java
@@ -1,5 +1,34 @@
 package com.normalizador.util.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class XmlParserTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testReadFile() {
+		fail("Not yet implemented");
+	}
 
 }
